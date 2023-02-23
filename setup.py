a4107528de807d9433258afad4028bfbd15a1a66
Mike Gray <mike@graywind.org>
2023-02-22 22:05:28 -0600

    chore: allow newer versions of Python
    Tested up to 3.11 without issues

--- setup.py
+++ setup.py
@@ -6,20 +6,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-rolldice",
-    version="0.4.0",
+    version="0.4.1",
     author="Finian Blackett",
     author_email="spamsuckersunited@gmail.com",
     description="A module for parsing dice notation",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/ThePlasmaRailgun/py-rolldice",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ),
     install_requires=[
-        'regex',
+        "regex",
     ],
 )
